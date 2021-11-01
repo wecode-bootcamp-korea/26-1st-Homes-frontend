2286bc6b5e9bb502ca67bb8799de1171fd830703
betterplaywon <kkgi1234@gmail.com>
2021-11-01 18:49:39 +0900

    꾸며줘 홈즈 초기 세팅 완료

---
+++ src/Routes.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+import Detail from './pages/Detail/Detail';
+import ProductLists from './pages/ProductLists/ProductLists';
+import Signup from './pages/Signup/Signup';
+
+export class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/detail" component={Detail} />
+          <Route exact path="/product-lists" component={ProductLists} />
+          <Route exact path="/signup" component={Signup} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
