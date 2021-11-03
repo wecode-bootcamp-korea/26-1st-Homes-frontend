bdef094110425a96e2329acace619daa5f30040c
saeyoung <syoungbb25@gmail.com>
2021-11-04 07:13:50 +0900

    mock data 진행중

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -10,50 +10,20 @@ class Login extends React.Component {
     };
   }
 
-  goToMain = () => {
-    this.props.history.push('./Main');
+  goToSinup = () => {
+    this.props.history.push('./Signup');
   };
 
-  handleemailInput = event => {
+  handlEmailInput = event => {
     this.setState({
       email: event.target.value,
     });
-    console.log('아이디input값>>', event.target.value);
   };
 
-  handelpwdInput = event => {
+  handelPwdInput = event => {
     this.setState({
       pwd: event.target.value,
     });
-    console.log('비밀번호input값>>', event.target.value);
-  };
-
-  // 로그인 버튼 클릭시 서버로 데이터 전송
-  handleSubmit = event => {
-    event.prevenDefault();
-
-    const loginInfo = {
-      method: 'POST',
-      body: JSON.stringify(this.state),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-    fetch('url', loginInfo)
-      .then(res => {
-        res.json();
-      })
-      .then(json => {
-        if (json.success === true) {
-          alert('로그인되었습니다.');
-          window.localStorage.setItem('userInfo', JSON.stringify(json));
-          this.setState({
-            email: json.email,
-            pwd: json.pwd,
-          });
-          this.props.history.push('/Main');
-        }
-      });
   };
 
   render() {
@@ -66,7 +36,7 @@ class Login extends React.Component {
           </div>
           <div className="viewbtnBox">
             <button className="viewBtn1">
-              <a href="">위코드</a>계정으로 로그인
+              <a href="https://wecode.co.kr/">위코드</a>계정으로 로그인
             </button>
             <button className="viewBtn2">카카오톡 계정으로 로그인</button>
             <button className="viewBtn3">네이버 계정으로 로그인</button>
@@ -79,21 +49,21 @@ class Login extends React.Component {
               className="email"
               type="email"
               placeholder="E-MAIL"
-              onChange={this.handleemailInput}
+              onChange={this.handlEmailInput}
             />
             <img id="emailIcon" src="./images/email.png" alt="이메일아이콘" />
             <input
               className="password"
               type="password"
               placeholder="PASSWORD"
-              onChange={this.handelpwdInput}
+              onChange={this.handelPwdInput}
             />
             <img id="pwdIcon" src="./images/padlock.png" alt="비밀번호아이콘" />
           </div>
           <div className="btnBox">
             <button
               id="loginBtn"
-              onClick={this.goToMain}
+              type="submit"
               disabled={
                 this.state.email.indexOf('@') !== -1 &&
                 this.state.pwd.length > 7
@@ -105,7 +75,9 @@ class Login extends React.Component {
             </button>
           </div>
           <div className="linkBox">
-            <span className="signupLink">회원가입</span>
+            <button className="signupLink" onClick={this.goToSinup}>
+              회원가입
+            </button>
             <span className="lostpwdLink">비밀번호 찾기</span>
           </div>
         </div>
