877ae6deb715713fc8636e725d380718b6e76a6a
양성호 <sungho@yangseonghoui-MacBookAir.local>
2021-11-04 20:01:11 +0900

    Add:중간 저장

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import './Detail.scss';
+
 export class Detail extends Component {
   constructor() {
     super();
     this.state = {
       productInfo: [],
+      buttonValue: true,
+      buttonDropDown: true,
     };
   }
-
+  buttonClick = event => {
+    this.setState({
+      buttonValue: this.state.buttonValue == true ? false : true,
+    });
+  };
   componentDidMount() {
-    fetch('http://localhost:3000/data/data.json', {
-      method: 'GET',
-    })
+    fetch('http:/data/data.json')
       .then(res => res.json())
       .then(data => {
         this.setState({
@@ -19,6 +24,13 @@ export class Detail extends Component {
         });
       });
   }
+
+  buttonClick = event => {
+    this.setState({
+      buttonValue: this.state.buttonValue == true ? false : true,
+    });
+  };
+
   render() {
     const { productInfo } = this.state;
     const costPrice = () => {
@@ -26,15 +38,16 @@ export class Detail extends Component {
         ((productInfo.cost * (1 - productInfo.percent)) / 10) * 10
       );
     };
+
     return (
-      <div className="detailPageBox">
-        <div className="ImageAndProduct">
+      <div className="Detail">
+        <div className="imageAndProduct">
           <img className="detailImage" alt="제품사진" src="/images/bed.jpg" />
           <div className="productInformation">
             <div className="brandName">{productInfo.brandName}</div>
             <div className="productName">{productInfo.productName}</div>
             <div className="productPrice">
-              <div className="costAndPercent">
+              <div>
                 <p className="cost">{productInfo.cost}</p>
                 <p className="percent">{productInfo.percent * 100}%</p>
               </div>
@@ -53,29 +66,78 @@ export class Detail extends Component {
             <div className="shippingFeeBox">
               <div className="shippingFee">배송비</div>
               <div className="shippingListsBox">
-                <ul className="shippingFeeLists">배송비 리스트</ul>
+                <ul className="shippingFeeLists">
+                  개당 {productInfo.shippingFee}원
+                </ul>
+                <li
+                  className={
+                    this.state.buttonValue == true
+                      ? 'shippingFeeNone'
+                      : 'shippingLists'
+                  }
+                >
+                  <div className="typeBox">
+                    <p className="shippingType">배송 타입</p>
+                    <p className="shippingTypeValue">
+                      {productInfo.shippingTypeValue}
+                    </p>
+                  </div>
+                  <div className="typeBox">
+                    <p className="payType">결제 방식</p>
+                    <p className="payTypeValue">{productInfo.payTypeValue}</p>
+                  </div>
+                </li>
               </div>
-              <bitton className="shippingFeeButton">∨</bitton>
+              <bitton className="shippingFeeButton" onClick={this.buttonClick}>
+                ∨
+              </bitton>
             </div>
             <div className="contour" />
             <div className="optionBox">
               <p>옵션 선택</p>
-              <div>
-                <button className="optionTop">
+              <div className="optionTop">
+                <div className="flexBox" onClick={this.buttonClick}>
                   <p className="firstLeftText">제품 선택</p>
                   <p className="firstRightText">∨</p>
-                </button>
+                </div>
+                <div className="firstOnOf">
+                  <div className="firstOptionHover">
+                    <div className="optionFlexBox ">
+                      <p className="optionLeftText">
+                        1. 캐더린 통수납 침대 SS 프레임만
+                      </p>
+                      <p className="optionRightText">189,000원~</p>
+                    </div>
+                  </div>
+                  <div className="secondOptionHover">
+                    <div className="optionFlexBox">
+                      <p className="optionLeftText">
+                        2. 캐더린린론 수납 침대 AS 프레임만
+                      </p>
+                      <p className="optionRightText">229,000원~</p>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div>
-                <button className="optionBottom">
+
+              <div className="optionBottom">
+                <div className="flexBox">
                   <p className="secondLeftText">색상 선택</p>
                   <p className="secondRightText">∨</p>
-                </button>
+                </div>
+                <div className="secondOnOff">
+                  <div className="optionFlexBox ">
+                    <p className="optionLeftText">1. 화이트</p>
+                  </div>
+                  <div className="optionFlexBox ">
+                    <p className="optionLeftText">2. 블루</p>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="buyButtons">
-              <button className="shoppingB1asket">장바구니</button>
-              <button className="shoppingBasket">바로구매</button>
+              <div className="buyButtons">
+                <button className="shoppingB1asket">장바구니</button>
+                <button className="shoppingBasket">바로구매</button>
+              </div>
             </div>
           </div>
         </div>
