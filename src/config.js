e24d2bd666d20ccbad2ceaba8283a4202349b615
saeyoung <syoungbb25@gmail.com>
2021-11-11 18:24:41 +0900

    config 수정

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-const CHAN_URL = 'http://10.58.0.131:8000';
-const MIN_URL = 'http://10.58.7.212:8000';
+export const CHAN_URL = 'http://10.58.0.131:8000';
+export const MIN_URL = 'http://10.58.7.212:8000';
 
-const API = {
-  signin: `{$MIN_URL}/signin`,
-  signup: `{$MIN_URL}/signup`,
+export const API = {
+  signin: `${MIN_URL}/signin`,
+  signup: `${MIN_URL}/signup`,
 };
