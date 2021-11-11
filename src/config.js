959749b4069229d9efdb7adc1f7dbcd177dd6195
saeyoung <syoungbb25@gmail.com>
2021-11-11 17:32:56 +0900

    사이더 반영 수정

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ const CHAN_URL = 'http://10.58.0.131:8000';
 const MIN_URL = 'http://10.58.7.212:8000';
 
 const API = {
-  signin: '${CHAN_URL}' / signin,
-  signup: '${CHAN_URL}' / signup,
+  signin: `{$MIN_URL}/signin`,
+  signup: `{$MIN_URL}/signup`,
 };
