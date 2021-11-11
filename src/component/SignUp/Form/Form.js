7ef8bf7a30a176a4fa9ee49c8fccee6f0156dcb3
saeyoung <syoungbb25@gmail.com>
2021-11-11 20:27:57 +0900

    congig 수정

--- src/component/SignUp/Form/Form.js
+++ src/component/SignUp/Form/Form.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Input from '../../Input';
 import { withRouter } from 'react-router-dom';
 import { SIGN_UP_FORMS } from '../data';
+import { MIN_URL } from '../../../config.js';
 import './Form.scss';
 
 class Form extends React.Component {
@@ -36,7 +37,7 @@ class Form extends React.Component {
     const { history } = this.props;
     const { email, nickName, phone_number, password, rePassword, name } =
       this.state;
-    fetch('http://10.58.7.212:8000/users/signup', {
+    fetch(`${MIN_URL}/users/signup`, {
       method: 'POST',
       body: JSON.stringify({
         email: email,
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const CHAN_URL = 'http://10.58.0.131:8000';
-export const MIN_URL = 'http://10.58.7.212:8000';
+export const MIN_URL = 'http://3.35.131.23:8000';
 
 export const API = {
   signin: `${MIN_URL}/signin`,
